4bfa570d306b0aca616f8625fc43e9d187aa3c91
Deepak Pradeep <dpac90@gmail.com>
2018-07-25 16:16:15 -0500

    Fixed Header Scroll issue
    - Rewrote script and put it on all HTML landing pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,8 @@
-/*
++/*
 	Strata by HTML5 UP
 	html5up.net | @n33co
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
-
-function showMenu() {
-    if (document.body.scrollTop > 50) //Show the top menu after scrolling down 50px
-        $('#nav-menu').fadeIn();
-    else
-        $('#nav-menu').fadeOut();
-}
 
 function showNavArrows() {
     if (document.body.scrollTop > 300) //Show the project navigation arrows after scrolling down 200px
