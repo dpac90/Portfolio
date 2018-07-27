99fc2940c6999063718bdd64a238810a9c6c5348
Deepak Pradeep <dpac90@gmail.com>
2018-07-26 22:07:55 -0500

    Fixed Image Viewer & Added Translate.com Bonus
    - Issue was with main.js file
    - Added YouTube Ad for Translate.com App

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,8 @@
-+/*
+/*
 	Strata by HTML5 UP
 	html5up.net | @n33co
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
-
-function showNavArrows() {
-    if (document.body.scrollTop > 300) //Show the project navigation arrows after scrolling down 200px
-        $('.projectNavArrow').fadeIn();
-    else
-        $('.projectNavArrow').fadeOut();
-}
 
 (function($) {
 
